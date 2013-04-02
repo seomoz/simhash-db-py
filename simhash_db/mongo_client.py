cc2f3d963ffc0c50e6f81e8c60573de84d17c0ab
Maura Hubbell <maura@seomoz.org>
2013-04-02 09:22:02 -0700

    FC-200: Retention policy for Simhash client for MongoDB.

--- simhash_db/mongo_client.py
+++ simhash_db/mongo_client.py
@@ -4,7 +4,14 @@
 
 import struct
 import pymongo
+from pymongo import common
 from . import BaseClient
+import calendar
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import dateutil.parser
+
+pymongo.common.VALIDATORS['days'] = pymongo.common.validate_positive_integer
 
 
 def unsigned_to_signed(integer):
@@ -14,7 +21,7 @@ def unsigned_to_signed(integer):
 
 def signed_to_unsigned(integer):
     '''Convert an unsigned integer into a signed integer with the same bits'''
-    return struct.unpack('!Q', struct.pack('!q', integer))[0]    
+    return struct.unpack('!Q', struct.pack('!q', integer))[0]
 
 
 class Client(BaseClient):
@@ -22,16 +29,45 @@ class Client(BaseClient):
     def __init__(self, name, num_blocks, num_bits, *args, **kwargs):
         BaseClient.__init__(self, name, num_blocks, num_bits)
         self.client = pymongo.Connection(*args, **kwargs)
-        self.name = name
-        self.docs = getattr(self.client, name).documents
+        self.days = kwargs.pop('days', None)
+        self.namePrefix = name + '-'
+        if self.days is None:
+            self.names = [name]
+            self.docsList = [getattr(self.client, name).documents]
+        else:
+            today = datetime.now()
+            self.names = [self.namePrefix
+                          + (today -
+                             relativedelta(days=i)).strftime(
+                                 '%Y-%m-%d')
+                          for i in range(self.days)]
+            self.docsList = [getattr(self.client, n).documents
+                             for n in self.names]
 
         # Create the indexes (if they exist it's ok)
-        for i in range(self.num_tables):
-            self.docs.create_index(str(i), pymongo.ASCENDING)
+        for j in range(len(self.names)):
+            for i in range(self.num_tables):
+                self.docsList[j].create_index(str(i), pymongo.ASCENDING)
 
     def delete(self):
         '''Delete this database of simhashes'''
-        self.client.drop_database(self.name)
+        for name in self.names:
+            self.client.drop_database(name)
+
+    def delete_old(self):
+        '''Delete data that's older than the retention period.'''
+        if self.days is None:
+            return
+
+        names = self.client.database_names()
+        today = datetime.now()
+        cutoff = today - relativedelta(days=self.days)
+        for name in names:
+            if name.startswith(self.namePrefix):
+                dbDateString = name[len(self.namePrefix):]
+                dbDate = dateutil.parser.parse(dbDateString)
+                if dbDate < cutoff:
+                    self.client.drop_database(name)
 
     def insert(self, hash_or_hashes):
         '''Insert one (or many) hashes into the database'''
@@ -47,20 +83,20 @@ class Client(BaseClient):
             ) for i in range(self.num_tables)) for hsh in hashes
         ]
         # And now insert them
-        self.docs.insert(docs)
+        self.docsList[0].insert(docs)
 
-    def find_in_table(self, hsh, table_num, ranges):
+    def find_in_table(self, docs, hsh, table_num, ranges):
         '''Return all the results found in this particular table'''
         low = unsigned_to_signed(ranges[table_num][0])
         high = unsigned_to_signed(ranges[table_num][1])
-        results = self.docs.find({str(table_num): {
+        results = docs.find({str(table_num): {
             '$gt': low,
             '$lt': high
         }})
         results = [self.corpus.tables[table_num].unpermute(
             signed_to_unsigned(int(d[str(table_num)]))) for d in results]
         return [h for h in results if
-            self.corpus.distance(h, hsh) < self.num_bits]
+                self.corpus.distance(h, hsh) < self.num_bits]
 
     def find_one(self, hash_or_hashes):
         '''Find one near-duplicate for the provided query (or queries)'''
@@ -72,10 +108,13 @@ class Client(BaseClient):
         for hsh in hashes:
             ranges = self.ranges(hsh)
             found = []
-            for i in range(self.num_tables):
-                found = self.find_in_table(hsh, i, ranges)
+            for docs in self.docsList:
+                for i in range(self.num_tables):
+                    found = self.find_in_table(docs, hsh, i, ranges)
+                    if found:
+                        results.append(found[0])
+                        break
                 if found:
-                    results.append(found[0])
                     break
             if not found:
                 results.append(None)
@@ -94,8 +133,9 @@ class Client(BaseClient):
         for hsh in hashes:
             ranges = self.ranges(hsh)
             found = []
-            for i in range(self.num_tables):
-                found.extend(self.find_in_table(hsh, i, ranges))
+            for docs in self.docsList:
+                for i in range(self.num_tables):
+                    found.extend(self.find_in_table(docs, hsh, i, ranges))
             found = list(set(found))
             results.append(found)
 
